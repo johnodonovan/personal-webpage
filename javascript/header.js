c4809ac2351831c1edb98b2761c34f732060b65f
John O'Donovan <jodmail@gmail.com>
2017-01-13 09:29:20 -0800

    header update

--- javascript/header.js
+++ javascript/header.js
@@ -8,7 +8,7 @@ document.write('				<div id="title_div">	');
 document.write('			<span id="name_span">John O\'Donovan </span>  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp ');
 document.write('			Department of Computer Science, University of California, Santa Barbara		');
 document.write('			 </div>');
-document.write('			 <div id="intro_text_div">I am an associate research scientist, principal investigator and lecturer at the Computer Science dept., UCSB. My research background is in AI, with focus on recommender systems.  I have a particular interest in modeling trust in social networks,  both at the data-mining layer and visualization-interaction layers. 	 I am also co-founder and CTO of <a href=http://www.sirassystems.com>Siras Systems Inc.</a>, providers of personalized educational planning and reporting.	  ');
+document.write('			 <div id="intro_text_div">I am an associate research scientist at the Computer Science dept., UCSB. My research background is in AI, with focus on recommender systems.  I have a particular interest in human-computer interaction aspects of these systems --for example, learning how a particular interaction design impacts human trust in an algorithm over time. 	I am also a co-founder and CTO of <a href=http://www.sirassystems.com>Siras Systems Inc.</a>, providers of personalized educational planning and reporting.	  ');
 document.write('			   <div id="ucsb_logo"><img src="images/logos/ucsb.png" alt="ucsb" />');
 document.write('			</div>');
 document.write('			 <div id="contact_text_div1">Email: jod@cs.ucsb.edu	  ');
